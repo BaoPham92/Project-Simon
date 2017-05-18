767e4be4902f44fdb42374a88ea3d9b08a779f72
Bao Cong Pham <BaoPham92@yahoo.com>
2017-05-18 10:09:34 -0400

    individual click buttons working

--- animation.js
+++ animation.js
@@ -1,32 +1,59 @@
-// setInterval(update, 50);
+// setInterval(update, 70);
 
 //Animations.
 
-// var chestList = {};
+// var classes = [".chestBlue", ".chestGreen", ".chestOrange", ".chestSelector"]
 
 var element = document.querySelector('.chestBlue');
 var chestBlue = new Motio(element, {
-  fps: 4,
+  fps: 7,
   frames: 7
 });
 chestBlue.toEnd();
+
 var element = document.querySelector('.chestGreen');
 var chestGreen = new Motio(element, {
-  fps: 4,
+  fps: 7,
   frames: 7
 });
 chestGreen.toEnd();
 
 var element = document.querySelector('.chestOrange');
 var chestOrange = new Motio(element, {
-  fps: 4,
+  fps: 7,
   frames: 7
 });
 chestOrange.toEnd();
 
+// var element = document.querySelector('.chestBlue1');
+// var chestBlue = new Motio(element, {
+//   fps: 7,
+//   frames: 7
+// });
+// chestBlue.toEnd();
 
+// var element = document.querySelector('.chestGreen1');
+// var chestGreen = new Motio(element, {
+//   fps: 7,
+//   frames: 7
+// });
+// chestGreen.toEnd();
+
+// var element = document.querySelector('.chestOrange1');
+// var chestOrange = new Motio(element, {
+//   fps: 7,
+//   frames: 7
+// });
+// chestOrange.toEnd();
+
+// var element = document.querySelector('.chestSelector');
+// var chestSelector = new Motio(element, {
+//   fps: 15,
+//   frames: 7
+// });
+// chestSelector.toEnd();
 // function updateEntity(anyChest){
-//   anyChest.play();
+//   anyChest.toEnd();
 // }
 
 // function update(){
--- script.js
+++ script.js
@@ -11,27 +11,86 @@
 	//Variable storage for text.
 	// var buttonText;
 	// var tempText;
+	var answerwer = [];
+	var tempAnswer = [];
 
 //This randomize the divs with any of the listed classes.
  function reset(){
 	$(document).ready(function(){
 		var classes = ["chestBlue", "chestGreen", "chestOrange"];
 
+		$(".divChestContainer div").removeClass("chestBlue chestGreen chestOrange")
+
 		$(".divChestContainer div").each(function(){
 			$(this).addClass(classes[Math.floor(Math.random() * classes.length)]);
+			 answerwer.push($(this).attr('class'))
 		});
 	});
 }
 reset();
 
-//Working on a click/compare buttom function below.
-	// $(".divButtons").on("click", function(){
-	// 	tempText = $(this).text()
+//  function reset1(){
+// 	$(document).ready(function(){
+// 		var classes = ["chestBlue1", "chestGreen1", "chestOrange1"];
 
-	// 	for (var index in chestText){
-	// 		if (temp === index) {
-	// 			console.log("You win!")
-	// 		}
-	// 	}
+// 		$(".divChestContainer1 div").removeClass("chestBlue1 chestGreen1 chestOrange1")
 
-	// });
+// 		$(".divChestContainer1 div").each(function(){
+// 			$(this).addClass(classes[Math.floor(Math.random() * classes.length)]);
+// 			answerwer.push($(this).attr('class'))
+// 		});
+// 	});
+// }
+// reset1();
+
+
+	$("#blueButton").on("click", function(){
+		tempAnswer.push($(".divChestContainer div[class='chestBlue']").attr('class'))
+		console.log(tempAnswer);
+			
+			function arraysEqual(tempAnswer, answer) {
+			    if(tempAnswer.length !== answer.length)
+			    	console.log("False")
+			        return false;
+			    for(var i = tempAnswer.length; i--;) {
+			        if(tempAnswer[i] !== answer[i])
+			            return false;
+			    }
+			    console.log("true");
+			    return true;
+			}
+	});
+
+				$("#greenButton").on("click", function(){
+			tempAnswer.push($(".divChestContainer div[class='chestGreen']").attr('class'))
+			console.log(tempAnswer);
+				
+				function arraysEqual(tempAnswer, answer) {
+				    if(tempAnswer.length !== answer.length)
+				        return false;
+				    for(var i = tempAnswer.length; i--;) {
+				        if(tempAnswer[i] !== answer[i])
+				            return false;
+				    }
+				    return true;
+				}
+		});
+
+					$("#orangeButton").on("click", function(){
+				tempAnswer.push($(".divChestContainer div[class='chestOrange']").attr('class'))
+				console.log(tempAnswer);
+					
+					function arraysEqual(tempAnswer, answer) {
+					    if(tempAnswer.length !== answer.length)
+					        return false;
+					    for(var i = tempAnswer.length; i--;) {
+					        if(tempAnswer[i] !== answer[i])
+					            return false;
+					    }
+					    console.log("true");
+					    return true;
+					}
+
+			});
+		
+	
